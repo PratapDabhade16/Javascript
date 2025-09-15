3e751e16d23f536fc8f36788212793888e1db3a1
Pratap_Dabhade16 <pratapdabhade343@gmail.com>
2025-09-15 14:17:50 +0530

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,11 @@
+//create a number variable num with some value 
+// now print good if the number is divisible by 10 and print bad if it is not 
+
+
+let num = 10 ;
+if(num % 10 == 0){
+console.log("good");
+}
+else{
+console.log("bad");
+}
