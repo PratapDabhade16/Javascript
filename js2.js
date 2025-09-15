5b14002797ff14b4f22971da768c672620c79ba0
Pratap_Dabhade16 <pratapdabhade343@gmail.com>
2025-09-15 14:39:29 +0530

    Update js2.js

--- js2.js
+++ js2.js
@@ -9,3 +9,36 @@ console.log("good");
 else{
 console.log("bad");
 }
+
+// take the user name and age as the prompts then retrun back the following statement to the suser as an alert
+// by substituing their name and age 
+
+let name = prompt("enter your name ");
+let age = prompt("enter your age ");
+
+alert("hello" +name+"you are" +age+ "years old");
+
+// write a statement to print the months in a quarter 
+
+let quarter = 1 ;
+switch(quarter){
+
+  case 1: console.log("january , february , march ");
+    break ;
+  case 2 : console.log("april , may , june ");
+    break ;
+  case 3 : console.log("july , august , september");
+    break ;
+  case 4 : console.log("october , november , december");
+    break ;
+  default : console.log("not a quarter");
+
+
+
+let str = "apples"
+if(str[0] == 'a' || str[0] == 'A' && str.length > 5){
+console.log("golden string");
+}
+    else{
+    console.log("not a golden string ")
+    }
